30ea2ed07be7504032919e91d27ac534e5bedd7a
Dylan DeAmaral <deamaral.dylan@gmail.com>
2017-12-10 12:31:51 -0500

    Added base foundation for the projects
    Created the basic visual studio projects for the site.

---
+++ MissingPets.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MissingPets.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
