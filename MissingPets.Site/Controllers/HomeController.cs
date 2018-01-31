135b1822c911d57f7685408b52fdd3c08903b330
Dylan DeAmaral <deamaral.dylan@gmail.com>
2018-01-30 20:08:36 -0500

    changed some of the html files and converted them to utilize razor. I replaced some of the links, fixed some of the resource tags, removed some of the graph project stuff (still a WIP)

--- MissingPets.Site/Controllers/HomeController.cs
+++ MissingPets.Site/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MissingPets.Site.Models;
 
@@ -12,6 +8,8 @@ namespace MissingPets.Site.Controllers
     {
         public IActionResult Index()
         {
+            // TODO: Figure out how to connect to Neo4J
+            //MissingPets.Data.Graph.GraphDriver driver = new Data.Graph.GraphDriver("localhost://7687", "", "");
             return View();
         }
 
@@ -26,6 +24,11 @@ namespace MissingPets.Site.Controllers
         {
             ViewData["Message"] = "Your contact page.";
 
+            return View();
+        }
+
+        public IActionResult Services()
+        {
             return View();
         }
 
